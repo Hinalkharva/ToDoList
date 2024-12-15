9566cc56afc802d18bab559c83086f4e85714843
Hinalkharva <hinalkharva5@gmail.com>
2024-12-16 01:18:24 +0530

    added CRUD operation and appIcon and dark, light mode into app .... my App is completed

---
+++ ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Hinal Rajubhai Kharva on 15/12/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
